527fbb2af817b37ef75e1a99fdebdba5e9e7ab89
lidong1665 <lidong1665@163.com>
2016-03-08 13:55:39 +0800

    fixed bug

--- Android-Rapid-Development-Of-Library/android_library/src/main/java/com/lidong/android_ibrary/switchlayout/FlipAnimation.java
+++ Android-Rapid-Development-Of-Library/android_library/src/main/java/com/lidong/android_ibrary/switchlayout/FlipAnimation.java
@@ -6,28 +6,18 @@ import android.view.animation.Animation;
 import android.view.animation.Transformation;
 
 /**
- * ����Ϊʹ�����˵���
- * 
- * @author 85204_000
  * 
  */
 public class FlipAnimation extends Animation {
-	/** ֵΪtrueʱ����ȷ�鿴��������ת���� */
 	public static final boolean DEBUG = false;
-	/** ��Y�������򿴣���ֵ��1ʱ������ʱ����ת�� */
 	public static final boolean ROTATE_DECREASE = true;
-	/** ��Y�������򿴣���ֵ��1ʱ����˳ʱ����ת�� */
 	public static final boolean ROTATE_INCREASE = false;
-	/** Z���������ȡ� */
 	public static final float DEPTH_Z = 310.0f;
-	/** ������ʾʱ���� */
 	public static final long DURATION = 800l;
-	/** ͼƬ��ת���͡� */
 	private final boolean type;
 	private final float centerX;
 	private final float centerY;
 	private Camera camera;
-	/** ���ڼ����������ȡ���ֵ����ʱ�����txtNumber�����ݡ� */
 	private InterpolatedTimeListener listener;
 
 	public FlipAnimation(float cX, float cY, boolean type) {
@@ -39,7 +29,6 @@ public class FlipAnimation extends Animation {
 
 	public void initialize(int width, int height, int parentWidth,
 			int parentHeight) {
-		// �ڹ��캯��֮��getTransformation()֮ǰ���ñ�������
 		super.initialize(width, height, parentWidth, parentHeight);
 		camera = new Camera();
 	}
@@ -50,7 +39,6 @@ public class FlipAnimation extends Animation {
 
 	protected void applyTransformation(float interpolatedTime,
 			Transformation transformation) {
-		// interpolatedTime:��������ֵ����ΧΪ[0.0f,10.f]
 		if (listener != null) {
 			listener.interpolatedTime(interpolatedTime);
 		}
@@ -65,10 +53,8 @@ public class FlipAnimation extends Animation {
 		float degree = from + (to - from) * interpolatedTime;
 		boolean overHalf = (interpolatedTime > 0.5f);
 		if (overHalf) {
-			// ��ת���������£�Ϊ��֤������Ϊ�ɶ������ֶ��Ǿ���Ч�������֣��跭ת180�ȡ�
 			degree = degree - 180;
 		}
-		// float depth = 0.0f;
 		float depth = (0.5f - Math.abs(interpolatedTime - 0.5f)) * DEPTH_Z;
 		final Matrix matrix = transformation.getMatrix();
 		camera.save();
@@ -82,13 +68,10 @@ public class FlipAnimation extends Animation {
 				matrix.postTranslate(centerX * 2, centerY);
 			}
 		} else {
-			// ȷ��ͼƬ�ķ�ת����һֱ������������ĵ�λ��
 			matrix.preTranslate(-centerX, -centerY);
 			matrix.postTranslate(centerX, centerY);
 		}
 	}
-
-	/** �������ȼ������� */
 	public static interface InterpolatedTimeListener {
 		public void interpolatedTime(float interpolatedTime);
 	}
